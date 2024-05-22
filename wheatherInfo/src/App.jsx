59d18bf42fa37e90b680d73bf91bdfd1d6360a67
ruchika123yadav <145842561+ruchika123yadav@users.noreply.github.com>
2024-05-23 00:01:22 +0530

    update

---
+++ wheatherInfo/src/App.jsx
@@ -0,0 +1,19 @@
+ 
+import './App.css'
+import SeachWheather from './searchWheather';
+import Wheather_Info from './Wheather_Info.jsx';
+
+function App() {
+
+  return (
+    <>
+    {/* < MaterialUi /> */}
+    <SeachWheather/>
+    <br />
+    <Wheather_Info/>
+
+    </>
+  )
+}
+
+export default App
